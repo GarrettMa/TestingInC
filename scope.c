e79efe5fab9893edf0da96941ffafc35e009e541
GarrettMa <55817010+GarrettMa@users.noreply.github.com>
2020-12-10 14:40:27 -0800

    Add files via upload

--- scope.c
+++ scope.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#define REPLACEMENT k =
 
 int main()
 {
-	int i=0;
-	int j=6;
+	int i = 0;
+	int j = 10;
+	int REPLACEMENT 20;
 	{
-		i=1;
-		int j=7;
-			
+		i = 1;
+		int j = 11;
 		{
-				i=2;
-				j=8;
+				i = 2;
+				j = 12;
 		}
 	}
 	printf("i: %d\n",i);
 	printf("j: %d\n",j);
+	printf("k: %d\n",k);
 	return 1;
 }
