ddda0d9498a81b2db1d753c5d87ff4f11c202d0e
GarrettMa <55817010+GarrettMa@users.noreply.github.com>
2020-12-10 14:27:21 -0800

    Add files via upload

--- funnycodegobrr.c
+++ funnycodegobrr.c
@@ -4,17 +4,22 @@ int main()
 {
 	int i=0;
 	{
+		int j=30;
 		{
+
 			i=20;
 			{
 				{
+					int j=40;
 					{
 						i=10;
+						j=20;
 					}
 				}
-				printf("%d\n",i);
+				printf("i: %d\n",i);
 			}
 		}
+		printf("j: %d\n",j);
 	}
 	return 1;
 }
